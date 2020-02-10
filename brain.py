fbf85a95353204d1f57b1a7255e5069462e26d02
matthew berntsen <mb99@shaw.ca>
2020-02-10 15:44:51 -0800

    update brain

--- brain.py
+++ brain.py
@@ -1,3 +1,12 @@
 
 
-def get_direction
+def make_board(data):
+    return [[]]
+
+def get_current_options(board, data):
+    return ['left']
+
+def get_direction(data):
+    data['board'] = make_board(data)
+    curr_options = get_current_options(data)
+    return curr_options[0]

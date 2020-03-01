ca6c1e725d43e60a3f0b6b887ce7019afd802bf6
matthew berntsen <mb99@shaw.ca>
2020-03-01 10:46:28 -0800

    asd

--- brain.py
+++ brain.py
@@ -7,5 +7,5 @@ def get_current_options(data):
     return ['left']
 
 def get_direction(board):
-    curr_options = get_current_options(data)
+    curr_options = get_current_options(board)
     return curr_options[0]

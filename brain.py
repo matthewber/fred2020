b458e796d4a8f4eb841a27089ed111d75e4a0903
matthew berntsen <mb99@shaw.ca>
2020-04-10 17:46:37 -0700

    release v.1.2.9

--- brain.py
+++ brain.py
@@ -10,18 +10,33 @@ def get_self_head(data):
     print(head)
     return head
 
-def get_adjacent_pieces(board, data):
-    self_head = get_self_head(data)
-    return False
+def get_move(direction, head):
+    x = update_x(head['x'], direction)
+    y = update_y(head['y'], direction)
+    return {'direction':direction, 'x':x, 'y':y}
 
+
+def get_move_options(board, data):
+    move_options = []
+    self_head = get_self_head(data)
+    directions = ['up', 'left', 'right']
+    for direction in directions:
+        move = get_move(direction, self_head)
+        move_options.append(move)
+    return move_options
+
+def is_valid_move(option, data):
+    return True
 
 def get_current_options(board, data):
-    adjacent_pieces = get_adjacent_pieces(board, data)
-    return ['right','left','up']
-
-
+    curr_options = []
+    options = get_move_options(board, data)
+    for option in options:
+        if is_valid_move(option, data):
+            curr_options.append(option)
+    return curr_options
 
 def get_direction(board, data):
     curr_options = get_current_options(board, data)
     print(curr_options)
-    return curr_options[0]
+    return curr_options[0]['direction']
